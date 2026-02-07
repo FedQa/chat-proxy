5f554c3aeef2b32a3042d95987f124d016e8d571
Fedor Rudnik <63184742+FedQa@users.noreply.github.com>
2026-02-07 22:01:03 +0500

    completions

---
+++ app/api/chat/completions/route.ts
@@ -0,0 +1,35 @@
+import {client_api} from "@/app/api/_lib/api";
+import {NextResponse} from "next/server";
+
+
+const ALLOWED_ORIGIN = '*';
+
+export async function POST(req: Request) {
+    try {
+        const requestBody = await req.json();
+
+        const {model, messages} = requestBody;
+
+        const client = client_api();
+
+        const completion = await client.chat.completions.create({
+            model,
+            messages,
+        });
+        console.log(completion.choices[0].message);
+
+
+        return NextResponse.json({
+            completion
+        })} catch(error) {
+        console.log(error);
+
+        return new Response(
+            JSON.stringify({ error: error instanceof Error ? error.message : 'An unknown error occurred' }),
+            {
+                status: 500,
+                headers: { 'Content-Type': 'application/json' },
+            }
+        )
+    }
+}
