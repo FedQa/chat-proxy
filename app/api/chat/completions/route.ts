ff301dff943f7c62c5d311aa28f4760b82be44b0
Fedor Rudnik <f.rudnik@stroki.dev>
2026-02-10 16:22:37 +0500

    gpt 4

--- app/api/chat/completions/route.ts
+++ app/api/chat/completions/route.ts
@@ -91,7 +91,7 @@ export async function POST(req: Request) {
         }`;
         const client = client_api();
         const completion = await client.chat.completions.create({
-            model: 'gpt-3.5-turbo',
+            model: 'gpt-4o-mini',
             messages: [
                 {role: "system", content: instruction},
                 {role: "user", content: messages},
